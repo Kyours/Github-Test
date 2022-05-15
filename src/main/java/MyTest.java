e28febb99fcc8d25557dd01957d217c4632f3958
Kyours <ma121254099@163.com>
2022-05-15 16:33:58 +0800

    second commit

---
+++ src/main/java/MyTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @author mmystart
+ * @create 2022-05-15 16:32
+ */
+public class MyTest {
+    @Test
+    public void test(){
+        System.out.println("hello github");
+    }
+
+}
